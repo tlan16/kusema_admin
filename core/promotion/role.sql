b6a03fac03e017301f4a6f552033996ec9842483
Frank Lan <franklan118@gmail.com>
2015-08-31 23:14:52 +1000

    new developer role

--- core/promotion/role.sql
+++ core/promotion/role.sql
@@ -1,3 +1,4 @@
 TRUNCATE TABLE `role`;
 INSERT INTO `role` (`id`, `name`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
+(1, 'Developer', 1, '2014-03-06 19:47:34', 10, '2014-03-06 08:47:34', 10),
 (5, 'Administrator', 1, '2014-03-06 19:47:34', 10, '2014-03-06 08:47:34', 10);
--- core/promotion/role_useraccount.sql
+++ core/promotion/role_useraccount.sql
@@ -1,3 +1,3 @@
 TRUNCATE TABLE `role_useraccount`;
 INSERT INTO `role_useraccount` (`roleId`, `useraccountId`, `created`, `createdById`) VALUES
-(5, 24, '2014-12-20 13:23:44', 20);
+(1, 24, '2014-12-20 13:23:44', 20);
