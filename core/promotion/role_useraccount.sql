da0130b6ed05fbe9b66c68e7766fbd807b630971
Frank Lan <franklan118@gmail.com>
2015-09-03 20:51:15 +1000

    new test user

--- core/promotion/role_useraccount.sql
+++ core/promotion/role_useraccount.sql
@@ -1,3 +1,4 @@
 TRUNCATE TABLE `role_useraccount`;
 INSERT INTO `role_useraccount` (`roleId`, `useraccountId`, `created`, `createdById`) VALUES
-(1, 24, '2014-12-20 13:23:44', 20);
+(1, 24, '2014-12-20 02:23:44', 20),
+(1, 25, '2015-09-03 10:43:17', 24);
