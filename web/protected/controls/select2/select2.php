42b19056d624d657f3c9c8baf4772bc2a7808ab0
Frank Lan <franklan118@gmail.com>
2015-09-03 12:32:12 +1000

    use cdn for select2

--- web/protected/controls/select2/select2.php
+++ web/protected/controls/select2/select2.php
@@ -17,12 +17,8 @@ class select2 extends TClientScript
 		if(!$this->getPage()->IsPostBack || !$this->getPage()->IsCallback)
 		{
 			$clientScript = $this->getPage()->getClientScript();
-			$folder = $this->publishFilePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR);
-			// Add jQuery library
-			// Add mousewheel plugin (this is optional)
-			$clientScript->registerHeadScriptFile('select2.js', $folder . '/select2.min.js');
-			$clientScript->registerStyleSheetFile('select2.css', $folder . '/select2.css', 'screen');
-			$clientScript->registerStyleSheetFile('select2.css.bootstrap', $folder . '/select2-bootstrap.css', 'screen');
+			$clientScript->registerHeadScriptFile('select2.js', "//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js");
+			$clientScript->registerStyleSheetFile('select2.css', "//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css", 'screen');
 		}
 	}
 }
