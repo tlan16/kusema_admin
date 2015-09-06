29b832a90640f43a6615e40f766d666e3ab6a098
Frank Lan <franklan118@gmail.com>
2015-09-06 16:51:53 +1000

    also fake answers, comment, units

--- web/cronjobs/test/fakeQna.php
+++ web/cronjobs/test/fakeQna.php
@@ -13,7 +13,6 @@ class fakeQna extends testAbstract
 			for($loop = 0; $loop < self::LOOPS; $loop++)
 			{
 				echo 'loop: ' . $loop . PHP_EOL;
-				$class = 'Question';
 				$title = self::getRandQuestion();
 				$content = self::getRandSentense(5,50);
 				$refId = md5($title.$content);
@@ -23,41 +22,85 @@ class fakeQna extends testAbstract
 				$topic = self::getRandObj('Topic');
 				$unit = self::getRandObj('Unit');
 				
-				$obj = $class::create($title, $content, $refId, $author, $authorName, $active);
+				$question = Question::create($title, $content, $refId, $author, $authorName, $active);
+				echo 'Question[' . $question->getId() . '] added' . PHP_EOL;
 				for($i = 0; $i < rand(1,5); $i++) {
-					$obj->addTopic(self::getRandObj('Topic'));
+					$question->addTopic(self::getRandObj('Topic'));
 				}
-				
-				echo $class . '[' . $obj->getId() . '] added, with ' . count($obj->getTopics()) . ' Topics' . PHP_EOL;
-				
-	// 			echo 'TESTING ' . $class . PHP_EOL;
-	// 			echo 'JSON: ' . PHP_EOL . print_r(self::getRealJson($obj->getJson()), true);
-				
-	// 			$class = 'Answer';
-	// 			$title = $class . '_title_' . self::getTimeString();
-	// 			$content = $class . '_content_' . self::getTimeString();
-	// 			$refId = $class . '_ref_' . self::getTimeString();
-	// 			$author = UserAccount::get(24);
-	// 			$authorName = 'alias of ' . $author->getPerson()->getFullName();
-	// 			$active = true;
-				
-	// 			$obj = $obj->addAnswer($title, $content, $refId, $author, $authorName, $active);
-				
-	// 			echo 'TESTING ' . $class . PHP_EOL;
-	// 			echo 'JSON: ' . PHP_EOL . print_r(self::getRealJson($obj->getJson()), true);
-				
-	// 			$class = 'Comments';
-	// 			$title = $class . '_title_' . self::getTimeString();
-	// 			$content = $class . '_content_' . self::getTimeString();
-	// 			$refId = $class . '_ref_' . self::getTimeString();
-	// 			$author = UserAccount::get(24);
-	// 			$authorName = 'alias of ' . $author->getPerson()->getFullName();
-	// 			$active = true;
-				
-	// 			$obj = $obj->addComments($title, $content, $refId, $author, $authorName, $active);
-				
-	// 			echo 'TESTING ' . $class . PHP_EOL;
-	// 			echo 'JSON: ' . PHP_EOL . print_r(self::getRealJson($obj->getJson()), true);
+				echo count($question->getTopics()) . ' Topics added to Question[' . $question->getId() . ']' . PHP_EOL;
+				for($i = 0; $i < rand(1,5); $i++) {
+					$question->addUnit(self::getRandObj('Unit'));
+				}
+				echo count($question->getUnits()) . ' Units added to Question[' . $question->getId() . ']' . PHP_EOL;
+				for($i = 0; $i < rand(1,5); $i++) {
+					$title = self::getRandQuestion();
+					$content = self::getRandSentense(5,50);
+					$refId = md5($title.$content);
+					$author = self::getRandObj('Person');
+					$authorName = self::getRandName();;
+					$active = true;
+					$comment = $question->addComments($title, $content, $refId, $author, $authorName, $active);
+					echo 'Comment[' . $comment->getId() . '] added to Question[' . $question->getId() . ']' . PHP_EOL;
+					for($i = 0; $i < rand(1,3); $i++) {
+						$title = self::getRandQuestion();
+						$content = self::getRandSentense(5,50);
+						$refId = md5($title.$content);
+						$author = self::getRandObj('Person');
+						$authorName = self::getRandName();;
+						$active = true;
+						$commentOfComment = $comment->addComments($title, $content, $refId, $author, $authorName, $active);
+						for($i = 0; $i < rand(1,2); $i++) {
+							$title = self::getRandQuestion();
+							$content = self::getRandSentense(5,50);
+							$refId = md5($title.$content);
+							$author = self::getRandObj('Person');
+							$authorName = self::getRandName();;
+							$active = true;
+							$commentOfComment->addComments($title, $content, $refId, $author, $authorName, $active);
+						}
+						echo count($comment->getComments()) . ' Comments added to Comment[' . $comment->getId() . '] which belongs to Question[' . $question->getId() . ']' . PHP_EOL;
+					}
+				}
+				for($i = 0; $i < rand(1,20); $i++) {
+					$title = self::getRandQuestion();
+					$content = self::getRandSentense(5,50);
+					$refId = md5($title.$content);
+					$author = self::getRandObj('Person');
+					$authorName = self::getRandName();;
+					$active = true;
+					$anwser = $question->addAnswer($title, $content, $refId, $author, $authorName, $active);
+					echo 'Answer[' . $anwser->getId() . '] added to Question[' . $question->getId() . ']' . PHP_EOL;
+					for($i = 0; $i < rand(1,5); $i++) {
+						$title = self::getRandQuestion();
+						$content = self::getRandSentense(5,50);
+						$refId = md5($title.$content);
+						$author = self::getRandObj('Person');
+						$authorName = self::getRandName();;
+						$active = true;
+						$comment = $anwser->addComments($title, $content, $refId, $author, $authorName, $active);
+						echo 'Comment[' . $comment->getId() . '] added to Anwser[' . $anwser->getId() . ']' . PHP_EOL;
+						for($i = 0; $i < rand(1,3); $i++) {
+							$title = self::getRandQuestion();
+							$content = self::getRandSentense(5,50);
+							$refId = md5($title.$content);
+							$author = self::getRandObj('Person');
+							$authorName = self::getRandName();;
+							$active = true;
+							$commentOfComment = $comment->addComments($title, $content, $refId, $author, $authorName, $active);
+							for($i = 0; $i < rand(1,2); $i++) {
+								$title = self::getRandQuestion();
+								$content = self::getRandSentense(5,50);
+								$refId = md5($title.$content);
+								$author = self::getRandObj('Person');
+								$authorName = self::getRandName();;
+								$active = true;
+								$commentOfComment->addComments($title, $content, $refId, $author, $authorName, $active);
+							}
+							echo count($commentOfComment->getComments()) . ' CommentOfComment added to Comment[' . $commentOfComment->getId() . '] which belongs to Answer[' . $anwser->getId() . ']' . PHP_EOL;
+						}
+						echo count($comment->getComments()) . ' Comments added to Answer[' . $anwser->getId() . ']' . PHP_EOL;
+					}
+				}
 			}
 			if($transStarted === false)
 				Dao::commitTransaction();
@@ -72,4 +115,6 @@ class fakeQna extends testAbstract
 	}
 }
 
+$time_start = microtime(true);
 fakeQna::run();
+echo PHP_EOL . 'Execution time for ' . fakeQna::LOOPS . ' loops: ' . date("H:i:s",microtime(true)-$time_start) . PHP_EOL;
