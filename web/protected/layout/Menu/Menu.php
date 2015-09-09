e5f2a46cde67901c1b85bc70092e7ef318871546
Frank Lan <franklan118@gmail.com>
2015-09-09 21:55:00 +1000

    url bug fix

--- web/protected/layout/Menu/Menu.php
+++ web/protected/layout/Menu/Menu.php
@@ -29,7 +29,7 @@ class Menu extends TTemplateControl
 				'topic' => array('url' => '/statics/topic.html', 'name' => 'Top Topics', 'icon' => ''),
 				'unit' => array('url' => '/statics/unit.html', 'name' => 'Top Units', 'icon' => ''),
 				'newquestion' => array('url' => '/statics/newquestion.html', 'name' => 'Current Year Question Count', 'icon' => ''),
-				'dailynewquestion' => array('url' => '/statics/newquestion.html', 'name' => 'Question Count By Time In A Day', 'icon' => '')
+				'dailynewquestion' => array('url' => '/statics/dailynewquestion.html', 'name' => 'Question Count By Time In A Day', 'icon' => '')
 			)
 		);
 		$html = "<ul class='nav navbar-nav'>";
