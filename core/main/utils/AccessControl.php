5c4e84c7b74c1c579284cc355e649596b9160085
Frank Lan <franklan118@gmail.com>
2015-09-29 10:10:15 +1000

    seperate access control info read and write for question detail page

--- core/main/utils/AccessControl.php
+++ core/main/utils/AccessControl.php
@@ -29,6 +29,17 @@ Abstract class AccessControl
 	{
 		switch($role->getId())
 		{
+			default:
+				{
+					return true;
+				}
+		}
+		return false;
+	}
+	public static function canEditQuestionDetailsPage(Role $role)
+	{
+		switch($role->getId())
+		{
 			case Role::ID_SYSTEM_DEVELOPER:
 			case Role::ID_ADMIN_USER:
 			case Role::ID_SYSTEM_ADMIN:
