5c7ddad3325604e6443a0a1f3c1add81403b66ea
Frank <tlan16@sina.com>
2015-10-04 00:15:01 +1000

    fit comments entity structure for this project

--- web/protected/controls/CommentsDiv/CommentsDiv.1.js
+++ web/protected/controls/CommentsDiv/CommentsDiv.1.js
@@ -1,1 +1,197 @@
-var CommentsDivJs=new Class.create;CommentsDivJs.prototype={SAVE_BTN_ID:"",_pageJs:null,initialize:function(e,t,n,o,s){this._pageJs=e,this._entityName=t,this._entityId=n,this._displayDivId=s,this._pageSize=o||5},_setDisplayDivId:function(e){var t={};return t.me=this,t.me._displayDivId=e,t.me},_getCommentsRow:function(e){var t={};return t.me=this,t.tag=e.id?"td":"th",t.newRow=new Element("tr",{"class":"comments_row"}).store("data",e).insert({bottom:new Element(t.tag,{"class":"col-xs-1"}).update(e.id?new Element("small").update(t.me._pageJs.loadUTCTime(e.created).toLocaleString()):e.created)}).insert({bottom:new Element(t.tag,{"class":"col-xs-1"}).update(e.id?new Element("small").update(e.createdBy.person.fullname):e.createdBy.person.fullname)}).insert({bottom:new Element(t.tag,{"class":"col-xs-1"}).update(e.id?new Element("small").update(e.type):e.type)}).insert({bottom:new Element(t.tag,{"class":""}).update(e.id?new Element("small").update(e.comments):e.comments)}),t.newRow},_getComments:function(e,t,n){var o={};return o.me=this,o.pageNo=e||1,o.loadingDiv=o.me._pageJs.getLoadingImg(),o.btn=n?$(n):void 0,o.ajax=new Ajax.Request("/ajax/getComments",{method:"get",parameters:{entity:o.me._entityName,entityId:o.me._entityId,orderBy:{created:"desc"},pageNo:e,pageSize:o.me._pageSize},onLoading:function(){1===o.pageNo&&$(t).update(o.loadingDiv),o.btn&&(o.me._pageJs._signRandID(o.btn),jQuery("#"+o.btn.id).button("loading"))},onSuccess:function(n){try{if(1===o.pageNo?$(t).update(""):$(t).down(".comments_get_more_btn_div")&&$(t).down(".comments_get_more_btn_div").remove(),o.tbody=$(t).down("tbody"),o.tbody||$(t).insert({bottom:new Element("table",{"class":"table table-condensed table-hover"}).insert({bottom:new Element("thead").update(o.me._getCommentsRow({type:"Type",createdBy:{person:{fullname:"WHO"}},created:"WHEN",comments:"COMMENTS"}))}).insert({bottom:o.tbody=new Element("tbody")})}),o.result=o.me._pageJs.getResp(n.responseText,!1,!0),!o.result||!o.result.items)return;o.result.items.each(function(e){o.tbody.insert({bottom:o.me._getCommentsRow(e)})}),o.result.pageStats.pageNumber<o.result.pageStats.totalPages&&o.tbody.insert({bottom:new Element("tr",{"class":"comments_get_more_btn_div"}).insert({bottom:new Element("td",{colspan:4}).insert({bottom:new Element("span",{"class":"btn btn-primary btn-xs","data-loading-text":"Getting More ..."}).update("Get More Comments").observe("click",function(){o.me._getComments(1*e+1,t,this)})})})})}catch(s){1===o.pageNo?$(t).insert({bottom:o.me.getAlertBox("ERROR: ",s).addClassName("alert-danger")}):o.me._pageJs.showModalBox('<strong class="text-danger">Error</strong>',s)}},onComplete:function(){o.loadingDiv.remove(),o.btn&&jQuery("#"+o.btn.id).button("reset")}}),this},_addComments:function(e,t){var n={};return n.me=this,n.commentsBox=$(e).up(".new_comments_wrapper").down("[new_comments=comments]"),n.comments=$F(n.commentsBox),n.comments.blank()?this:(n.me._pageJs.postAjax(CommentsDivJs.SAVE_BTN_ID,{comments:n.comments,entityId:n.me._entityId,entityName:n.me._entityName},{onLoading:function(){jQuery("#"+e.id).button("loading")},onSuccess:function(e,o){try{if(n.result=n.me._pageJs.getResp(o,!1,!0),!n.result||!n.result.item||!n.result.item.id)return;n.tbody=$(t).down("tbody"),n.tbody||$(t).insert({bottom:n.tbody=new Element("tbody")}),n.tbody.insert({top:n.me._getCommentsRow(n.result.item)}),n.commentsBox.setValue("")}catch(s){n.me._pageJs.showModalBox('<strong class="text-danger">Error</strong>',s)}},onComplete:function(){jQuery("#"+e.id).button("reset")}}),this)},_getEmptyCommentsDiv:function(e){var t={};return t.me=this,t.newDiv=new Element("div",{"class":"row new_comments_wrapper"}).insert({bottom:new Element("div",{"class":"col-xs-2 text-right"}).update("<strong>New Comments:</strong>")}).insert({bottom:new Element("div",{"class":"col-xs-10"}).insert({bottom:new Element("div",{"class":"input-group"}).insert({bottom:new Element("input",{"class":"form-control",type:"text",new_comments:"comments",placeholder:"add more comments to this order"}).observe("keydown",function(e){t.me._pageJs.keydown(e,function(){$(e.currentTarget).up(".new_comments_wrapper").down("[new_comments=btn]").click()})})}).insert({bottom:new Element("span",{"class":"input-group-btn"}).insert({bottom:new Element("span",{id:"add_new_comments_btn",new_comments:"btn","class":"btn btn-primary","data-loading-text":"saving..."}).update("add").observe("click",function(){t.me._addComments(this,e)})})})})}),t.newDiv},render:function(){var e={};e.me=this,e.newDiv=new Element("div",{"class":"panel panel-default"}).insert({bottom:new Element("div",{"class":"panel-heading"}).update("Comments:")}).insert({bottom:e.resultDiv=new Element("div",{"class":"comments_result_list table-responsive"})}).insert({bottom:new Element("div",{"class":"panel-footer"}).update(e.me._getEmptyCommentsDiv(e.resultDiv))}),$(e.me._displayDivId).update(e.newDiv),e.me._getComments(1,e.resultDiv)}};
+/**
+ * The display Comments div
+ */
+var CommentsDivJs = new Class.create();
+CommentsDivJs.prototype = {
+	SAVE_BTN_ID: ''
+	,_pageJs: null
+	,_titleRow: {'author': {'fullName': 'Author'}, 'createdBy': {'person': {'fullname': 'WHO'}}, 'created': 'WHEN', 'content': 'Content'}
+
+	//constructor
+	,initialize : function(_pageJs, _entityName, _entityId, _pageSize, _displayDivId, _noHeading) {
+		this._pageJs = _pageJs;
+		this._entityName = _entityName;
+		this._entityId = _entityId;
+		this._displayDivId = _displayDivId;
+		this._pageSize = (_pageSize || 5);
+		this._noHeading = (_noHeading || true);
+	}
+	/**
+	 * setting the display div id
+	 */
+	,_setDisplayDivId: function (_displayDivId) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.me._displayDivId = _displayDivId;
+		return tmp.me;
+	}
+	/**
+	 * Getting the comments row
+	 */
+	,_getCommentsRow: function(comments) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.tag = comments.id ? 'td' : 'th';
+		tmp.newRow =  new Element('tr', {'class': 'comments_row'})
+			.store('data', comments)
+			.insert({'bottom': new Element(tmp.tag, {'class': 'col-xs-1'}).update(!comments.id ? comments.created : new Element('small').update(tmp.me._pageJs.loadUTCTime(comments.created).toLocaleString() ) ) })
+			.insert({'bottom': new Element(tmp.tag, {'class': 'col-xs-1'}).update(!comments.id ? comments.author.fullName : new Element('small').update(comments.author.fullName) ) })
+			.insert({'bottom': new Element(tmp.tag, {'class': ''}).update(!comments.id ? comments.content : new Element('small').update( comments.content) ) })
+			;
+		return tmp.newRow;
+	}
+	/**
+	 * Ajax: getting the comments into the comments div
+	 */
+	,_getComments: function (pageNo, resultDivId, btn) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.pageNo = (pageNo || 1);
+		tmp.loadingDiv = tmp.me._pageJs.getLoadingImg();
+		tmp.btn = (btn ? $(btn) : undefined);
+		tmp.ajax = new Ajax.Request('/ajax/getComments', {
+			method: 'get'
+			,parameters: {'entity': tmp.me._entityName, 'entityId': tmp.me._entityId, 'orderBy': {'created':'desc'}, 'pageNo': pageNo, 'pageSize': tmp.me._pageSize}
+			,onLoading: function() {
+				if(tmp.pageNo === 1) {
+					$(resultDivId).update(tmp.loadingDiv);
+				}
+				if(tmp.btn) {
+					tmp.me._pageJs._signRandID(tmp.btn);
+					jQuery('#' + tmp.btn.id).button('loading');
+				}
+			}
+			,onSuccess: function(transport) {
+				try {
+					if(tmp.pageNo === 1) {
+						$(resultDivId).update('');
+					} else {
+						//remove the pagination btn
+						if($(resultDivId).down('.comments_get_more_btn_div')) {
+							$(resultDivId).down('.comments_get_more_btn_div').remove();
+						}
+					}
+					tmp.tbody = $(resultDivId).down('tbody');
+					if(!tmp.tbody) {
+						$(resultDivId).insert({'bottom': new Element('table', {'class': 'table table-condensed table-hover'})
+							.insert({'bottom': new Element('thead').update(tmp.me._getCommentsRow( tmp.me._titleRow ) ) })
+							.insert({'bottom': tmp.tbody = new Element('tbody') })
+						});
+					}
+					tmp.result = tmp.me._pageJs.getResp(transport.responseText, false, true);
+					if(!tmp.result || !tmp.result.items)
+						return;
+
+					//add new data
+					tmp.result.items.each(function(item) {
+						tmp.tbody.insert({'bottom': tmp.me._getCommentsRow(item) });
+					})
+					//who new pagination btn
+					if(tmp.result.pageStats.pageNumber < tmp.result.pageStats.totalPages) {
+						tmp.tbody.insert({'bottom': new Element('tr', {'class': 'comments_get_more_btn_div'})
+							.insert({'bottom': new Element('td', {'colspan': 4})
+								.insert({'bottom': new Element('span', {'class': 'btn btn-primary btn-xs', 'data-loading-text': 'Getting More ...'})
+									.update('Get More Comments')
+									.observe('click', function(){
+										tmp.me._getComments(pageNo * 1 + 1, resultDivId, this);
+									})
+								})
+							})
+						})
+					}
+				} catch (e) {
+					if(tmp.pageNo === 1) {
+						$(resultDivId).insert({'bottom': tmp.me.getAlertBox('ERROR: ', e).addClassName('alert-danger') });
+					} else {
+						tmp.me._pageJs.showModalBox('<strong class="text-danger">Error</strong>', e);
+					}
+				}
+			}
+			,onComplete: function() {
+				tmp.loadingDiv.remove();
+				if(tmp.btn) {
+					jQuery('#' + tmp.btn.id).button('reset');
+				}
+			}
+		});
+		return this;
+	}
+	/**
+	 * Ajax: adding a comments to this order
+	 */
+	,_addComments: function(btn, resultDiv) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.commentsBox = $(btn).up('.new_comments_wrapper').down('[new_comments=comments]');
+		tmp.comments = $F(tmp.commentsBox);
+		if(tmp.comments.blank())
+			return this;
+		tmp.me._pageJs.postAjax(CommentsDivJs.SAVE_BTN_ID, {'comments': tmp.comments, 'entityId': tmp.me._entityId, 'entityName': tmp.me._entityName}, {
+			'onLoading': function(sender, param) {
+				jQuery('#' + btn.id).button('loading');
+			}
+			,'onSuccess': function (sender, param) {
+				try {
+					tmp.result = tmp.me._pageJs.getResp(param, false, true);
+					if(!tmp.result || !tmp.result.item || !tmp.result.item.id)
+						return;
+					tmp.tbody = $(resultDiv).down('tbody');
+					if(!tmp.tbody)
+						$(resultDiv).insert({'bottom': tmp.tbody = new Element('tbody') });
+					tmp.tbody.insert({'top': tmp.me._getCommentsRow(tmp.result.item)})
+					tmp.commentsBox.setValue('');
+				} catch (e) {
+					tmp.me._pageJs.showModalBox('<strong class="text-danger">Error</strong>', e);
+				}
+			}
+			,'onComplete': function () {
+				jQuery('#' + btn.id).button('reset');
+			}
+		})
+		return this;
+	}
+	/**
+	 * Getting a empty comments div
+	 */
+	,_getEmptyCommentsDiv: function(resultDiv) {
+		var tmp = {};
+		tmp.me = this;
+		tmp.newDiv = new Element('div', {'class': 'row new_comments_wrapper'})
+			.insert({'bottom': new Element('div', {'class': 'col-xs-2 text-right'}).update('<strong>New Comments:</strong>') })
+			.insert({'bottom': new Element('div', {'class': 'col-xs-10'})
+				.insert({'bottom': new Element('div', {'class': 'input-group'})
+					.insert({'bottom': new Element('input', {'class': 'form-control', 'type': 'text', 'new_comments': 'comments', 'placeholder': 'add more comments to this order'})
+						.observe('keydown', function(event) {
+							tmp.me._pageJs.keydown(event, function() {
+								$(event.currentTarget).up('.new_comments_wrapper').down('[new_comments=btn]').click();
+							});
+						})
+					})
+					.insert({'bottom': new Element('span', {'class': 'input-group-btn'})
+						.insert({'bottom': new Element('span', {'id': 'add_new_comments_btn', 'new_comments': 'btn', 'class': 'btn btn-primary', 'data-loading-text': 'saving...'})
+							.update('add')
+							.observe('click', function() {
+								tmp.me._addComments(this, resultDiv);
+							})
+						})
+					})
+				})
+			});
+		return tmp.newDiv;
+	}
+	/**
+	 * render the div
+	 */
+	,render: function() {
+		var tmp = {};
+		tmp.me = this;
+		tmp.newDiv = new Element('div', {'class': 'panel panel-default'})
+			.insert({'bottom': new Element('div', {'class': 'panel-heading'}).update('Comments:') })
+			.insert({'bottom': tmp.resultDiv = new Element('div', {'class': 'comments_result_list table-responsive'})})
+			.insert({'bottom': new Element('div', {'class': 'panel-footer'}).update(tmp.me._getEmptyCommentsDiv( tmp.resultDiv )) });
+		if(tmp.me._noHeading === true)
+			tmp.newDiv.down('.panel-heading').hide();
+		$(tmp.me._displayDivId).update(tmp.newDiv);
+		tmp.me._getComments(1,  tmp.resultDiv);
+	}
+}
