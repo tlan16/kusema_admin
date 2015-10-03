919c8ad04d85d3688bb580162992011f49ec74ee
Frank <tlan16@sina.com>
2015-10-03 22:07:34 +1000

    markdown.js is requred

--- web/protected/controls/bootstrapMarkdown/bootstrapMarkdown.php
+++ web/protected/controls/bootstrapMarkdown/bootstrapMarkdown.php
@@ -20,6 +20,7 @@ class bootstrapMarkdown extends TClientScript
 // 			$folder = $this->publishFilePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR);
 			// Add jQuery library
 			// Add mousewheel plugin (this is optional)
+			$clientScript->registerScriptFile('markdown.js', "https://cdnjs.cloudflare.com/ajax/libs/markdown.js/0.5.0/markdown.min.js");
 			$clientScript->registerScriptFile('bstp_markdown.js', "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.9.0/js/bootstrap-markdown.min.js");
 			$clientScript->registerStyleSheetFile('bstp_markdown.css', "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.9.0/css/bootstrap-markdown.min.css", 'screen');
 		}
