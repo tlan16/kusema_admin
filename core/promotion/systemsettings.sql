73c3620707b7698a383f53e08827bd4903a72a83
Frank <tlan16@sina.com>
2015-10-08 12:06:31 +1100

    use https for API

--- core/promotion/systemsettings.sql
+++ core/promotion/systemsettings.sql
@@ -1,3 +1,3 @@
 TRUNCATE TABLE `systemsettings`;
 INSERT INTO `systemsettings` (`id`, `type`, `value`, `description`, `active`, `created`, `createdById`, `updated`, `updatedById`) VALUES
-(1, 'forum_api_rest', 'http://melts-dev.eng.monash.edu:8003/rest/', '', 1, '2015-09-16 00:57:44', 10, '2015-09-15 14:57:44', 10);
+(1, 'forum_api_rest', 'https://melts-dev.eng.monash.edu:8003/rest/', '', 1, '2015-09-16 00:57:44', 10, '2015-09-15 14:57:44', 10);
