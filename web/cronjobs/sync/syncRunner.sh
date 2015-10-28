6df238f5735c2935159c0cdd55026185d36f7225
Frank <tlan16@sina.com>
2015-10-28 11:13:29 +1100

    sync more

--- web/cronjobs/sync/syncRunner.sh
+++ web/cronjobs/sync/syncRunner.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-php $DIR/pullUser.php 
-php $DIR/pullUnit.php
+php $DIR/pullPerson.php
 php $DIR/pullTopic.php
+php $DIR/pullUnit.php
+php $DIR/pullQuestion.php
+php $DIR/pullAnswer.php
+php $DIR/pullComments.php
